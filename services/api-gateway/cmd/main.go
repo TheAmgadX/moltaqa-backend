0b8c222127886f938915872dcea7e7ea42ca720c
TheAmgadX <theamgadx@gmail.com>
2026-07-06 20:17:07 +0300

    feat: add the foundation of the api-gateway service

--- services/api-gateway/cmd/main.go
+++ services/api-gateway/cmd/main.go
@@ -1,17 +1,104 @@
 package main
 
 import (
-	"fmt"
+	"context"
+	"errors"
+	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/TheAmgadX/moltaqa-backend/shared/middlewares"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
-func main() {
-	mux := http.NewServeMux()
+func addMiddlewares(router *chi.Mux) {
+	router.Use(middleware.Logger)
+	router.Use(middlewares.CORSMiddleware)
+}
 
-	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "Hello, World!")
+func defineRoutes(router *chi.Mux) {
+	router.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello, World!"))
 	})
+}
 
-	fmt.Println("Listenning to port 8080")
-	http.ListenAndServe(":8080", mux)
+func createServer() *http.Server {
+	router := chi.NewRouter()
+
+	addMiddlewares(router)
+
+	defineRoutes(router)
+
+	return &http.Server{
+		Addr:    ":8080",
+		Handler: router,
+	}
+}
+
+func gracefulShutdown(server *http.Server, shutdownTimeout time.Duration) error {
+	shutdownCtx, cancel := context.WithTimeout(context.Background(), shutdownTimeout)
+	defer cancel()
+
+	if err := server.Shutdown(shutdownCtx); err != nil {
+		// server graceful shutdown has failed, close the server `Not Gracefully`
+		log.Printf("failed during shutting down the server gracefully: %v\n", err)
+
+		if closeErr := server.Close(); closeErr != nil {
+			log.Printf("failed during closing the server: %v\n", closeErr)
+			return errors.Join(err, closeErr)
+		}
+
+		return err
+	}
+
+	log.Println("Server Graceful Shutdown done successfully.")
+
+	return nil
+}
+
+func runServer(server *http.Server, shutdownTimeout time.Duration, ctx context.Context) error {
+	serverErrChan := make(chan error, 1)
+
+	// run the server
+	go func() {
+		if err := server.ListenAndServe(); !errors.Is(err, http.ErrServerClosed) {
+			serverErrChan <- err
+		}
+
+		close(serverErrChan)
+	}()
+
+	stopChan := make(chan os.Signal, 1)
+
+	signal.Notify(stopChan, os.Interrupt, syscall.SIGTERM)
+
+	// wait for signals
+	select {
+	case <-stopChan:
+		log.Println("received a stop signal")
+
+	case <-serverErrChan:
+		log.Println("server error:", <-serverErrChan)
+
+	case <-ctx.Done():
+		log.Println("context done:", <-ctx.Done())
+	}
+
+	// handle server shutdown
+	return gracefulShutdown(server, shutdownTimeout)
+}
+
+func main() {
+	log.Println("Start api-gateway service...")
+
+	server := createServer()
+
+	if err := runServer(server, 10*time.Second, context.Background()); err != nil {
+		log.Printf("Failed while running the server: %v", err)
+		return
+	}
 }
---
+++ shared/middlewares/cors.go
@@ -0,0 +1,30 @@
+package middlewares
+
+import "net/http"
+
+var allowedOrigins = map[string]struct{}{
+	"http://localhost:3000": {},
+}
+
+func CORSMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		origin := r.Header.Get("Origin")
+		if _, ok := allowedOrigins[origin]; !ok {
+			next.ServeHTTP(w, r)
+			return
+		}
+
+		w.Header().Set("Access-Control-Allow-Origin", origin)
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Authorization, Content-Type")
+		w.Header().Set("Access-Control-Allow-Credentials", "true")
+		w.Header().Set("Vary", "Origin")
+
+		if r.Method == http.MethodOptions {
+			w.WriteHeader(http.StatusNoContent)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
