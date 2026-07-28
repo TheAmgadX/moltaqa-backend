a3999f7439c780469f99becdf486823e7ee6389b
Mahmoud Shaban Amer <m7mud.xd.gamer2@gmail.com>
2026-07-28 14:10:58 +0300

    Update the constraints

--- services/user-service/migrations/000001_init_schema.up.sql
+++ services/user-service/migrations/000001_init_schema.up.sql
@@ -1,22 +1,30 @@
 CREATE TABLE users (
     id UUID PRIMARY KEY,
 
-    username TEXT NOT NULL UNIQUE,
-    phone_number TEXT,
-    email TEXT UNIQUE,
+    username VARCHAR(30) NOT NULL UNIQUE,
+    phone TEXT UNIQUE NULL,
+    email TEXT UNIQUE NULL,
 
-    profile_image_url TEXT,
-    bio TEXT,
-    display_name TEXT,
+    profile_image_url TEXT NULL,
+    bio VARCHAR(250) DEFAULT '',
+    display_name VARCHAR(50),
 
     email_verified TIMESTAMP NULL,
     phone_verified TIMESTAMP NULL,
 
-    birth_date DATE,
+    birth_date DATE NULL,
 
-    bio_status TEXT,
+    bio_status VARCHAR(50) DEFAULT '',
 
-    account_badge TEXT NOT NULL DEFAULT 'UNVERIFIED',
+    account_badge TEXT NOT NULL DEFAULT 'unverified'
+        CHECK (
+            account_badge IN (
+                'unverified',
+                'blue_badge',
+                'golden_badge',
+                'silver_badge'
+            )
+        ),
 
     friends_count INTEGER NOT NULL DEFAULT 0,
     followers_count INTEGER NOT NULL DEFAULT 0,
@@ -31,10 +39,45 @@ CREATE TABLE users (
 CREATE TABLE privacy_settings (
     user_id UUID PRIMARY KEY,
 
-    avatar_visibility TEXT NOT NULL DEFAULT 'EVERYONE',
-    phone_visibility TEXT NOT NULL DEFAULT 'EVERYONE',
-    email_visibility TEXT NOT NULL DEFAULT 'EVERYONE',
-    last_seen_visibility TEXT NOT NULL DEFAULT 'EVERYONE',
+    avatar_visibility TEXT NOT NULL DEFAULT 'everyone'
+        CHECK (
+            avatar_visibility IN (
+                'everyone',
+                'friends',
+                'contacts',
+                'nobody'
+            )
+        ),
+
+    phone_visibility TEXT NOT NULL DEFAULT 'everyone'
+        CHECK (
+            phone_visibility IN (
+                'everyone',
+                'friends',
+                'contacts',
+                'nobody'
+            )
+        ),
+
+    email_visibility TEXT NOT NULL DEFAULT 'everyone'
+        CHECK (
+            email_visibility IN (
+                'everyone',
+                'friends',
+                'contacts',
+                'nobody'
+            )
+        ),
+
+    last_seen_visibility TEXT NOT NULL DEFAULT 'everyone'
+        CHECK (
+            last_seen_visibility IN (
+                'everyone',
+                'friends',
+                'contacts',
+                'nobody'
+            )
+        ),
 
     read_receipts_enabled BOOLEAN NOT NULL DEFAULT TRUE,
     find_by_username BOOLEAN NOT NULL DEFAULT TRUE,
